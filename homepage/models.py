938084af4dec7adc8de90c48517187c7427922ce
Rafael <faelleonan@gmail.com>
2020-03-23 10:20:35 -0300

    Banco criado e listando dados do banco no template

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField('Nome', max_length=150)
+    data_nasc = models.DateField('Data de nascimento', auto_now=False , auto_now_add=False)
+    foto = models.ImageField('Foto',upload_to='img')
+    email  = models.EmailField('Email', max_length=254)
+
+    def __str__(self):
+        return self.nome
--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Usuario
+
 
 def index(request):
-    return render(request, "index.html")
+    usuario = Usuario.objects.all()
+    return render(request, "index.html", {"usuarios":usuario})
+
+def delete(request, id):
+	usuario = get_object_or_404(Usuario, pk=id)
+	usuario.delete()
+	return redirect('/')
